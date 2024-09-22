b69f8b84350e905a7c7730238e5095f31d18d739
Taha <Taha@DESKTOP-SGO0H8A>
2024-09-22 10:07:25 +0300

    Department Controlloer Index(Action-View)

---
+++ MVC Company/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using Company.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Company.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository )
+        {
+            _departmentRepository = departmentRepository;
+        }
+        public IActionResult Index()
+        {
+            var department = _departmentRepository.GetAll();
+            return View(department);
+        }
+    }
+}
--- MVC Company/Program.cs
+++ MVC Company/Program.cs
@@ -1,4 +1,5 @@
 using Company.Data.Contexts;
+using Company.Data.Models;
 using Company.Repository.Interfaces;
 using Company.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -17,10 +18,11 @@ namespace MVC_Company
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
-            builder.Services.AddScoped<IEmployeeRepository, IEmployeeRepository>();
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            builder.Services.AddScoped<IGenericRepository<BaseEntity> , GenericRepository<BaseEntity>>();
 
-            var app = builder.Build();
+            var app = builder.Build(); 
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
