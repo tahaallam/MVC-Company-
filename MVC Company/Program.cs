a89476571f2b32c16361988d9ce08a3fb1286716
Taha <Taha@DESKTOP-SGO0H8A>
2024-09-26 17:55:56 +0300

    edit

--- MVC Company/Program.cs
+++ MVC Company/Program.cs
@@ -32,7 +32,7 @@ namespace MVC_Company
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts();   
             }
 
             app.UseHttpsRedirection();
